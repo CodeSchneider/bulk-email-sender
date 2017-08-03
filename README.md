5453e3c35de8516f0f30cc0fd2b9caed66fae794
Trey Schneider <treyschneider@LPATLICLTSCHN7.local>
2017-08-03 11:20:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mass-email-sender
+
+a [Sails](http://sailsjs.org) application
---
+++ api/models/User.js
@@ -0,0 +1,65 @@
+var uuid = require('node-uuid');
+
+module.exports = {
+
+  autoPK: false,
+
+  attributes: {
+
+    id: {
+        type: 'string',
+        primaryKey: true,
+        defaultsTo: function (){ return uuid.v4(); },
+        unique: true,
+        index: true,
+        uuidv4: true
+    },
+
+    firstName: {
+      type: 'string',
+      required: true
+    },
+
+    lastName: {
+      type: 'string',
+      required: true
+    },
+
+    position: {
+      type: 'string',
+      required: true
+    },
+
+    company: {
+      type: 'string',
+      required: true
+    },
+
+    industry: {
+      type: 'string',
+      required: false
+    },
+
+    companySize: {
+      type: 'string',
+      required: false
+    },
+
+    emailAddress: {
+      type: 'email',
+      required: true
+    },
+
+    website: {
+      type: 'string',
+      required: true
+    },
+
+    linkedinURL: {
+      type: 'string',
+      required: false
+    }
+
+  }
+
+};
